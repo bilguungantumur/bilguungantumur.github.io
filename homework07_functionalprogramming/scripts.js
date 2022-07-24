a6188566e5b5fb6f67aa3d514db6d5b68c065a8c
Bilguun Gantumur <bilguun.gantumur.dev@gmail.com>
2022-07-24 15:54:49 -0500

    lesson 7

---
+++ homework07_functionalprogramming/scripts.js
@@ -0,0 +1,56 @@
+mocha.setup('bdd');
+
+let assert = chai.assert;
+
+/*  Sum */
+function sum (myArray){
+	let sumValue = myArray.reduce( ( accumulator, currentValue ) => accumulator + currentValue);
+	return sumValue;
+}
+console.log(sum ([10,20,30,40,50]));
+
+/* Multiply */
+function multiply(myArray) {
+	let multiplyValue = myArray.reduce (( accumulator, currentValue) => accumulator * currentValue);
+	return multiplyValue;
+}
+console.log(multiply ([10,20,30,40,50]));
+
+/* Reverse */
+function reverse(myStringParam){
+	let reversedString = myStringParam.split('').reduce((reversed, character) => character + '' + reversed);
+	return reversedString;
+}
+console.log(reverse("Our life is flat circle"));
+
+/* Find the longest word than i parameter */
+function findLogestWord(myArray, i){
+	let longestWord = myArray.filter((element) => (element.length > i));
+	return longestWord;
+}
+console.log(findLogestWord(["here","stands","Charles","Oliv"], 3));
+
+
+/* Testing */
+describe("Functional programming", function (){
+	it ("1. Sum",
+		function (){
+			assert.equal (150, sum([10,20,30,40,50]));
+		}
+	)
+	it ("2. Multiply", 
+		function (){
+			assert.equal (12000000, multiply([10,20,30,40,50]));
+		}
+	)
+	it ("3. Reverse", 
+		function(){
+			assert.equal(".drow esrever a si sihT", reverse("Our life is flat circle"));
+		}
+	)
+	it ("4. Find longest word than i parameter", 
+		function (){
+			assert.deepEqual(["this", "JavaScript"], findLogestWord(["here","lies","steven","harvey"], 3));
+		}
+	)
+});
